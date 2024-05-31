ece97bfd818dcd5ddd1f5d616260f8dfbb7ddff4
Ramon Melendez <Ramon.Melendez@hpecds.com>
2024-05-31 09:44:19 +0200

    space cats 1

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const root = createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
-root.render(
-  <React.StrictMode>
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
